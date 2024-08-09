62b8d9a411d2043c60b173e02eecd9617d43a968
arnaudbore <arnaud.bore@gmail.com>
2024-08-09 17:07:46 -0400

    getting closer

--- src/main/java/com/scilus/nf/test/nifti/Methods.java
+++ src/main/java/com/scilus/nf/test/nifti/Methods.java
@@ -1,13 +1,51 @@
 package com.scilus.nf.test.nifti;
 
 import com.ericbarnhill.niftijio.*;
+import com.ericbarnhill.niftijio.tools.*;
 import java.io.IOException;
+import java.util.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class Methods
 {
-    public static String compare(String i_file_1) throws IOException
+    public static String compare(String i_file) throws IOException
     {
-        Nifti1Header f1 = Nifti1Header.read(i_file_1);
-    	return f1.filename.toString();
+
+        NiftiVolume v = NiftiVolume.read(i_file);
+        Nifti1Header f1 = v.getHeader1();
+        NDimensionalArray d = v.getData();
+
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<int[]> indcs = new IndexIterator().iterateReverse(d.getDims());
+
+        for (int[] indc: indcs) {
+            list.add(String.valueOf(d.get(indc)));
+        }
+        
+        Set<String> set = new HashSet<>(list);
+        list.clear();
+        list.addAll(set);
+                
+        String r = list.stream().collect(Collectors.joining("_"));
+        
+        f1.filename = f1.filename.replace("\\", "/");
+        int index = f1.filename.lastIndexOf("/");
+        f1.filename = f1.filename.substring(index + 1);
+
+        byte[] bytesOfMessage = r.getBytes("UTF-8");
+        String md5 = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] theMD5digest = md.digest(bytesOfMessage);
+            md5 = new String(theMD5digest, StandardCharsets.UTF_8);
+        }
+        catch (NoSuchAlgorithmException e) {
+            System.err.println("I'm sorry, but MD5 is not a valid message digest algorithm");
+        }
+        
+       	return md5;
     }
 }
