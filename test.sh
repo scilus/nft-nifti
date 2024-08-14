9542d73aa53117e63b7960a79e81182034e9b433
Arnaud Bore <arnaud.bore@gmail.com>
2024-08-14 11:33:28 -0400

    Update test.sh
    Co-authored-by: Alex Valcourt Caron <alexvc91@gmail.com>

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/Downloads/nf-test test tests/**/*.nf.test --plugins target/nft-nifti-*.jar --verbose --updateSnapshot tests/copy_nifti
+nf-test test tests/**/*.nf.test --plugins target/nft-nifti-*.jar --verbose --updateSnapshot tests/copy_nifti
