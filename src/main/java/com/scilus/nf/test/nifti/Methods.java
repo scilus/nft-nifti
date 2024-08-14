ee7451e37ba5fd15dad2d3d9c17806cd597c439a
arnaudbore <arnaud.bore@gmail.com>
2024-08-14 11:19:02 -0400

    add dummy nifti, cbange method

--- src/main/java/com/scilus/nf/test/nifti/Methods.java
+++ src/main/java/com/scilus/nf/test/nifti/Methods.java
@@ -10,11 +10,11 @@ import java.nio.charset.StandardCharsets;
 
 public class Methods
 {
-    public static String compare(String i_file) throws IOException
+    public static String md5sum(String i_file) throws IOException
     {
 
         NiftiVolume v = NiftiVolume.read(i_file);
-        Nifti1Header f1 = v.getHeader1();
+        Nifti1Header h = v.getHeader1();
         NDimensionalArray d = v.getData();
 
         ArrayList<int[]> indcs = new IndexIterator().iterateReverse(d.getDims());
@@ -27,12 +27,12 @@ public class Methods
             }
 
             // Replace filename with basename and remove descrip
-            f1.filename = f1.filename.replace("\\", "/");
-            int index = f1.filename.lastIndexOf("/");
-            f1.filename = f1.filename.substring(index + 1);
-            f1.descrip = new StringBuffer("");
+            h.filename = h.filename.replace("\\", "/");
+            int index = h.filename.lastIndexOf("/");
+            h.filename = h.filename.substring(index + 1);
+            h.descrip = new StringBuffer("");
         
-            md.update(f1.toString().replace("\0","").getBytes("UTF-8"));
+            md.update(h.toString().replace("\0","").getBytes("UTF-8"));
             byte[] theMD5digest = md.digest();
             md5 = new String(theMD5digest, StandardCharsets.UTF_8);
         }
@@ -40,6 +40,6 @@ public class Methods
             System.err.println("I'm sorry, but MD5 is not a valid message digest algorithm");
         }       	
 
-        return md5.replace("\0", "").replaceAll("[(){}]", "");
+        return h.filename + ":md5," + md5.replace("\0", "").replaceAll("[(){}]", "");
     }
 }
