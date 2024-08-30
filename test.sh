0825662f3b7c6bf0560ffe68fbba8826fccb04f5
Alex Valcourt Caron <alexvc91@gmail.com>
2024-08-30 15:16:03 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nf-test test tests/**/*.nf.test --plugins target/nft-nifti-0.0.1-rc5.jar --verbose tests/copy_nifti
+nf-test test tests/**/*.nf.test --plugins target/nft-nifti-*.jar --verbose tests/copy_nifti
